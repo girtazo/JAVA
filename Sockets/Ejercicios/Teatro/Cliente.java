6a93bdcdadffeb0dbea76de71182f3651d84e81e
girtazo <girtazo@gmail.com>
2015-01-23 00:15:44 +0100

    teatro

--- Sockets/Ejercicios/Teatro/Cliente.java
+++ Sockets/Ejercicios/Teatro/Cliente.java
@@ -7,74 +7,84 @@ class Cliente {
 	static final String HOST = "localhost";
 	static final int PUERTO = 5000;
 	private Scanner lector = new Scanner(System.in);
+	private Socket conexion;
+	private String mensaje = "";
+	private String respuesta = "";
 	public Cliente( ) {
 
 		try{
 
-			String mensaje;
-
 			//crear el socket
-			Socket skCliente = new Socket(HOST, PUERTO);
-			
-
-			//asocio flujo salida de datos al socket
-			OutputStream Outaux = skCliente.getOutputStream();
-				
-			//asocio flujo de datos flujo de tipo DataOutputStream 
-			DataOutputStream Outflujo = new DataOutputStream( Outaux );
-
-			Outflujo.writeUTF( lector.nextLine() );
-			//recojo flujo de datos del socket
-			InputStream Inaux = skCliente.getInputStream();
-
-			//asocio flujo de datos flujo de tipo DataInputStream
-			DataInputStream Influjo = new DataInputStream( Inaux );
-			
-			//Capturamos cadena del flujo con readUTF y muestro
-			System.out.println( Influjo.readUTF() );
-
-			while(true){
-
-				mensaje = scan.nextLine();
-
-				//asocio flujo salida de datos al socket
-				Outaux = skCliente.getOutputStream();
-				
-				//asocio flujo de datos flujo de tipo DataOutputStream 
-				Outflujo = new DataOutputStream( Outaux );
-			
-				Outflujo.writeUTF( mensaje );
-
-				if( mensaje.equalsIgnoreCase( "Adios" ) ) {
-
-					Inaux = skCliente.getInputStream();
-
-					//asocio flujo de datos flujo de tipo DataInputStream
-					Influjo = new DataInputStream( Inaux );
-			
-					//Capturamos cadena del flujo con readUTF y muestro
-					System.out.println( Influjo.readUTF() );
-					break;
-
-				}
-				Inaux = skCliente.getInputStream();
-
-				//asocio flujo de datos flujo de tipo DataInputStream
-				Influjo = new DataInputStream( Inaux );
-			
-				//Capturamos cadena del flujo con readUTF y muestro
-				System.out.println( Influjo.readUTF() );
-
-			}
-			//Cierro el socket
-			skCliente.close();
+			this.conexion = new Socket(HOST, PUERTO);
+		
 		}catch(Exception e) {
 
 		System.out.println( e.getMessage() );
 		
+		}
 	}
+
+	public void realizarConexion(){
+
+		escritura("Hola");
+		lectura();
 	}
-public static void main(String[] arg) {
-new Cliente();
-}
+
+	public void interaccion(){
+		try {
+		
+			while(true){
+
+				mensaje = lector.nextLine();
+
+				escritura(mensaje);
+				respuesta = lectura();
+
+				if( mensaje.equalsIgnoreCase( "Fin" ) ) {
+
+					conexion.close();
+					break;
+
+				}
+			}
+		}catch( Exception e ) {
+
+			System.out.println( e.getMessage() );
+		}
+	}
+	public String lectura() {
+
+		try {
+
+			InputStream input = this.conexion.getInputStream();
+			DataInputStream dataInput = new DataInputStream( input );
+			mensaje = dataInput.readUTF();
+			System.out.println("Servidor: " + mensaje);
+		}catch( Exception e ) {
+
+			System.out.println( e.getMessage() );
+		}
+		return mensaje;
+	}
+
+	public void escritura( String mensaje ){
+
+		try {
+		
+			OutputStream output = conexion.getOutputStream();
+			DataOutputStream dataOutput = new DataOutputStream( output );
+			dataOutput.writeUTF( mensaje );
+			System.out.println(mensaje);
+		}catch( Exception e ) {
+
+			System.out.println( e.getMessage() );
+		}
+	}
+
+	public static void main(String[] arg) {
+		
+		Cliente cliente = new Cliente();
+		cliente.realizarConexion();
+		cliente.interaccion();
+	}
 }
--- Sockets/Ejercicios/Teatro/Servidor.java
+++ Sockets/Ejercicios/Teatro/Servidor.java
@@ -25,143 +25,248 @@ class Servidor {
 	//puerto espera conexiones
 	static final int PUERTO = 5000;
 	private String[] mensajes = new String[5];
+	private ServerSocket socketServer;
+	private Socket conexion;
+	private Butaca[] cenButacas;
+	private Butaca[] lat1Butacas;
+	private Butaca[] lat2Butacas;
+	private Butaca[] galButacas;
+	private Butaca[] vip1Butacas;
+	private Butaca[] vip2Butacas;
+	private int cenButacasLibres;
+	private int lat1ButacasLibres;
+	private int lat2ButacasLibres;
+	private int galButacasLibres;
+	private int vip1ButacasLibres;
+	private int vip2ButacasLibres;
 
 	public Servidor( ) {
 
 		try {
 
-			mensajes[0] = "Esperando conexión...";
-			mensajes[1] = "Conectado.";
-			mensajes[2] = "Hola, soy el servidor.";
-			mensajes[3] = "Muy bien.";
-			mensajes[4] =  "Hasta luego";
-			
 			//crea socket 
-			ServerSocket skServidor = new ServerSocket( PUERTO );
+			this.socketServer = new ServerSocket( PUERTO );
+
+			// Asignar Cantidad de butacas
+			cenButacasLibres 	= 54;
+			lat1ButacasLibres 	= 4;
+			lat2ButacasLibres 	= 4;
+			galButacasLibres 	= 8;
+			vip1ButacasLibres 	= 3;
+			vip2ButacasLibres 	= 3;
+
+			// Preparar Arrays
+			this.cenButacas 	= new Butaca[cenButacasLibres];
+			this.lat1Butacas 	= new Butaca[lat1ButacasLibres];
+			this.lat2Butacas 	= new Butaca[lat2ButacasLibres];
+			this.galButacas 	= new Butaca[galButacasLibres];
+			this.vip1Butacas 	= new Butaca[vip1ButacasLibres];
+			this.vip2Butacas 	= new Butaca[vip2ButacasLibres];
 			
+			for( int n = 0; n < cenButacasLibres; n++ ) { 
 
-			while(true) { 
+				cenButacas[n] = new Butaca( "CEN", n, 80 );
 
-				// Esperando conexiones
-				System.out.println(mensajes[0]);
-				Socket conexion = skServidor.accept();
-				System.out.println( mensajes[1] );
+			}
 
-				// Esperando Inicio del cliente
-				InputStream input = conexion.getInputStream();
-				DataInputStream dataInput = new DataInputStream( input );
-			
-				if( ! dataInput.readUTF( ).equalsIgnoreCase( "Hola" ) ) {
+			for( int n = 0; n < lat1ButacasLibres; n++ ) { 
 
-					conexion.close();
+				lat1Butacas[n] = new Butaca( "LAT1", n, 100 );
 
-				}
+			}			
 
-				// Envio respuesta al cliente como aviso de conexion
-				OutputStream output = conexion.getOutputStream();
-				
-				DataOutputStream dataOutput = new DataOutputStream( output );
-			
-				dataOutput.writeUTF( mensajes[2] );
+			for( int n = 0; n < lat2ButacasLibres; n++ ) { 
+
+				lat2Butacas[n] = new Butaca( "LAT2", n, 100 );
 
 			}
 			
+			for( int n = 0; n < galButacasLibres; n++ ) { 
 
-			// while( true ) {
-				
-			// 	//recojo flujo de datos del socket
-			// 	input = conexion.getInputStream();
+				galButacas[n] = new Butaca( "GAL", n, 150 );
 
-			// 	//asocio flujo de datos flujo de tipo DataInputStream
-			// 	data = new DataInputStream( input );
+			}
 
-			// 	String mensaje_client = data.readUTF();
-			// 	System.out.println( mensaje_client );
+			for( int n = 0; n < vip1ButacasLibres; n++ ) { 
 
+				vip1Butacas[n] = new Butaca( "VIP1", n, 250 );
 
-			// 	//asocio flujo salida de datos al socket
-			// 	out_aux = conexion.getOutputStream();
-				
-			// 	//asocio flujo de datos flujo de tipo DataOutputStream 
-			// 	out_flujo= new DataOutputStream( out_aux );
-				
-			// 	if( mensaje_client.equalsIgnoreCase( "Adios" ) ) {
+			}
 
-			// 		out_flujo.writeUTF( mensajes[4] );
-					
-			// 		//cierro conexión
-			// 		conexion.close();
+			for( int n = 0; n < vip2ButacasLibres; n++ ) { 
 
-			// 	} else {
+				vip2Butacas[n] = new Butaca( "VIP2", n, 250 );
 
-			// 		out_flujo.writeUTF( mensajes[3] );
-			// 		//escribo
-					
+			}
 
-			// 	}
-				
-
-			// }
+			// Mensajes del servidor
+			mensajes[0] = "Esperando conexión...";
+			mensajes[1] = "Conectado.";
+			mensajes[2] = "Hola, soy el servidor.";
+			mensajes[3] = "Elige una opcion:\n";
+			mensajes[3] += "Reserva (tipoButaca) (númeroButaca)\n";
+			mensajes[3] += "Ver Butacas\n";
+			mensajes[3] += "Fin\n";
+			mensajes[4] =  "Hasta luego";
 
 		} catch( Exception e ) {
 
 			System.out.println( e.getMessage() );
 
 		}
+	}
+	public void listen() {
 
+		try {
+
+			String mensaje = "";
+			while(true) { 
+
+				// Esperando conexiones
+				System.out.println(mensajes[0]);
+				this.conexion = socketServer.accept();
+				
+				System.out.println( mensajes[1] );
+			
+				if( ! lectura().equalsIgnoreCase( "Hola" ) ) {
+
+					conexion.close();
+
+				}
+
+				escritura(mensajes[2]);
+				while(true){
+
+					escritura(mensajes[3]);
+					
+
+
+					if( lectura() == "Ver Butacas" ) {
+
+						mensaje = "Butacas de tipo CEN hay "+cenButacasLibres+" libres con un precio de "+ cenButacas[1].precio +"€\n";
+						mensaje += "Butacas de tipo LAT1 hay "+lat1ButacasLibres+" libres con un precio de "+ lat1Butacas[1].precio +"€\n";
+						mensaje += "Butacas de tipo LAT2 hay "+lat2ButacasLibres+" libres con un precio de "+ lat2Butacas[1].precio +"€\n";
+						mensaje += "Butacas de tipo GAL hay "+galButacasLibres+" libres con un precio de "+ galButacas[1].precio +"€\n";
+						mensaje += "Butacas de tipo VIP1 hay "+vip1ButacasLibres+" libres con un precio de "+ vip1Butacas[1].precio +"€\n";
+						mensaje += "Butacas de tipo VIP2 hay "+vip2ButacasLibres+" libres con un precio de "+ vip2Butacas[1].precio +"€\n";
+					}
+
+				}
+
+			}
+
+		} catch( Exception e ) {
+
+			System.out.println( e.getMessage() );
+
+		}
+	}
+
+	public String lectura() {
+
+		String mensaje = "";
+		try {
+
+			InputStream input = this.conexion.getInputStream();
+			DataInputStream dataInput = new DataInputStream( input );
+			mensaje = dataInput.readUTF();
+			System.out.println( "Cliente: " + mensaje );
+		}catch( Exception e ) {
+
+			System.out.println( e.getMessage() );
+		}
+		return mensaje;
+	}
+
+	public void escritura( String mensaje ){
+
+		try {
+		
+			OutputStream output = conexion.getOutputStream();
+			DataOutputStream dataOutput = new DataOutputStream( output );
+			dataOutput.writeUTF( mensaje );
+		}catch( Exception e ) {
+
+			System.out.println( e.getMessage() );
+		}
+	}
+
+	public Butaca[] getCenButacas() {
+		return this.cenButacas;
+	}
+
+	public Butaca[] getLat1Butacas() {
+		return this.lat1Butacas;
+	}
+
+	public Butaca[] getLat2Butacas() {
+		return this.lat2Butacas;
+	}
+
+	public Butaca[] getGalButacas() {
+		return this.galButacas;
+	}
+
+	public Butaca[] getVip1Butacas() {
+		return this.vip1Butacas;
+	}
+
+	public Butaca[] getVip2Butacas() {
+		return this.vip2Butacas;
+	}
+
+	public int getCenButacasLibres() {
+		return this.cenButacasLibres;
+	}
+
+	public void setCenButacasLibres(int butacas) {
+		this.cenButacasLibres = butacas;
+	}
+
+	public int getLat1ButacasLibres() {
+		return this.lat1ButacasLibres;
+	}
+
+	public void setLat1ButacasLibres(int butacas) {
+		this.lat1ButacasLibres = butacas;
+	}
+
+	public int getLat2ButacasLibres() {
+		return this.lat2ButacasLibres;
+	}
+
+	public void setLat2ButacasLibres(int butacas) {
+		this.lat2ButacasLibres = butacas;
+	}
+	public int getGalButacasLibres() {
+		return this.galButacasLibres;
+	}
+
+	public void setGalButacasLibres(int butacas) {
+		this.galButacasLibres = butacas;
+	}
+
+	public int getVip1ButacasLibres() {
+		return this.vip1ButacasLibres;
+	}
+
+	public void setVip1ButacasLibres(int butacas) {
+		this.vip1ButacasLibres = butacas;
+	}
+
+	public int getVip2ButacasLibres() {
+		return this.vip2ButacasLibres;
+	}
+
+	public void setVip2ButacasLibres(int butacas) {
+		this.vip2ButacasLibres = butacas;
 	}
 
 	public static void main( String[] arg ) {
+		
+		Servidor servidor = new Servidor();
 
-		Butaca[] cenButacas = new Butaca[54];
-
-		for( int n = 0; n < 54; n++ ) { 
-
-			cenButacas[n] = new Butaca( "CEN", n, 80 );
-
-		}
-
-		Butaca[] lat2Butacas = new Butaca[4];
-
-		for( int n = 0; n < 4; n++ ) { 
-
-			lat2Butacas[n] = new Butaca( "LAT2", n, 100 );
-
-		}
-
-		Butaca[] lat1Butacas = new Butaca[4];
-
-		for( int n = 0; n < 4; n++ ) { 
-
-			lat1Butacas[n] = new Butaca( "LAT1", n, 100 );
-
-		}
-
-		Butaca[] galButacas = new Butaca[8];
-
-		for( int n = 0; n < 8; n++ ) { 
-
-			galButacas[n] = new Butaca( "GAL", n, 150 );
-
-		}
-
-		Butaca[] vip1Butacas = new Butaca[3];
-
-		for( int n = 0; n < 3; n++ ) { 
-
-			vip1Butacas[n] = new Butaca( "VIP1", n, 250 );
-
-		}
-
-		Butaca[] vip2Butacas = new Butaca[3];
-
-		for( int n = 0; n < 3; n++ ) { 
-
-			vip2Butacas[n] = new Butaca( "VIP2", n, 250 );
-
-		}
-
-		new Servidor();
+		servidor.listen();
 
 
 	}
